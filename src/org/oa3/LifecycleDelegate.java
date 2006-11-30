95b7ad299c4070c32afd8df607e7a33a5efc4869
fred <fred@c0ec5643-5123-0410-b01b-e101a1959115>
2006-11-30 16:53:15 +0000

    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@11 c0ec5643-5123-0410-b01b-e101a1959115

---
+++ src/org/oa3/LifecycleDelegate.java
@@ -0,0 +1,113 @@
+/*
+ * [[
+ * Copyright (C) 2001 - 2006 The Software Conservancy as Trustee. All rights
+ * reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Nothing in this notice shall be deemed to grant any rights to
+ * trademarks, copyrights, patents, trade secrets or any other intellectual
+ * property of the licensor or any contributor except as expressly stated
+ * herein. No patent license is granted separate from the Software, for
+ * code that you delete from the Software, or for combinations of the
+ * Software with other software or hardware.
+ * ]]
+ */
+
+package org.oa3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LifecycleDelegate {
+
+	private static Log log = LogFactory.getLog(LifecycleDelegate.class);
+	
+	private ILifecycleComponent component;
+	private State currentState;
+	private List listeners = new ArrayList();
+	private Object LOCK = new Object();
+	private Object WAIT = new Object();
+	
+	public LifecycleDelegate(final ILifecycleComponent component, final State state) {
+		this.component = component;
+		this.currentState = state;
+	}
+	
+	public void addListener(ILifecycleListener listener) {
+		synchronized (LOCK) {
+			if (!listeners.contains(listener)) {
+				listeners.add(listener);
+			}
+		}
+	}
+
+	public void removeListener(ILifecycleListener listener) {
+		synchronized (LOCK) {
+			if (listeners.contains(listener)) {
+				listeners.remove(listener);
+			}
+		}
+	}
+
+	public void notifyListeners(State newState) {
+		synchronized (LOCK) {
+			for (Iterator iter = listeners.iterator(); iter.hasNext();) {
+				ILifecycleListener listener = (ILifecycleListener) iter.next();
+				try {
+					listener.stateChanged(component, newState);
+				} catch (RuntimeException e) {
+					log.error("unexpected exception", e);
+				}
+			}
+		}
+	}
+	
+	public boolean isState(State state) {
+		return currentState.equals(state);
+	}
+
+	public void setState(State state) {
+		synchronized (WAIT) {
+			currentState = state;
+			WAIT.notifyAll();
+		}
+	}
+	
+	public State getState() {
+		return currentState;
+	}
+	
+	public void waitForState(State state) {
+		while (currentState != state) {
+			synchronized (WAIT) {
+				try {
+					WAIT.wait();
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+
+}
