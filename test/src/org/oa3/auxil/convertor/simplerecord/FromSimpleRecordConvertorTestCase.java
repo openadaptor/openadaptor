a8ce171c3ca3732ecf329e7d3a0f23ccc499e83d
kscully <kscully@c0ec5643-5123-0410-b01b-e101a1959115>
2006-12-19 12:08:28 +0000

    Rudimentary tests for to/from simplerecord convertors.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@144 c0ec5643-5123-0410-b01b-e101a1959115

---
+++ test/src/org/oa3/auxil/convertor/simplerecord/FromSimpleRecordConvertorTestCase.java
@@ -0,0 +1,80 @@
+/*
+ * [[
+ * Copyright (C) 2001 - 2006 The Software Conservancy as Trustee. All rights
+ * reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Nothing in this notice shall be deemed to grant any rights to
+ * trademarks, copyrights, patents, trade secrets or any other intellectual
+ * property of the licensor or any contributor except as expressly stated
+ * herein. No patent license is granted separate from the Software, for
+ * code that you delete from the Software, or for combinations of the
+ * Software with other software or hardware.
+ * ]]
+ */
+package org.oa3.auxil.convertor.simplerecord;
+
+import org.jmock.MockObjectTestCase;
+import org.jmock.Mock;
+import org.oa3.auxil.simplerecord.ISimpleRecord;
+/*
+ * File: $Header: $
+ * Rev:  $Revision: $
+ * Created Dec 19, 2006 by oa3 Core Team
+ */
+
+public class FromSimpleRecordConvertorTestCase extends MockObjectTestCase {
+
+  protected FromSimpleRecordConvertor testSubject;
+  protected Mock mockIncomingSimpleRecord;
+
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    testSubject = createTestSubject();
+    mockIncomingSimpleRecord = new Mock(ISimpleRecord.class);
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    testSubject = null;
+    mockIncomingSimpleRecord = null;
+  }
+
+  protected FromSimpleRecordConvertor createTestSubject() {
+    FromSimpleRecordConvertor convertor = new FromSimpleRecordConvertor();
+    return convertor;
+  }
+
+  // Tests
+  public void testConvert() {
+    Object convertedObject = new Object();
+    mockIncomingSimpleRecord.expects(once()).method("getRecord").will(returnValue(convertedObject));
+
+    try {
+      testSubject.convert(mockIncomingSimpleRecord.proxy());
+    }
+    catch(Exception e) {
+      fail("Unexpected Exception: " + e);
+    }
+
+  }
+}
