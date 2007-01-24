0886e090cffa2cf9f0eb60f2a46a0a137d3cf0bf
kscully <kscully@c0ec5643-5123-0410-b01b-e101a1959115>
2007-01-24 16:15:00 +0000

    Partitioning tests.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@225 c0ec5643-5123-0410-b01b-e101a1959115

---
+++ test/integration/src/org/openadaptor/core/adaptor/ConnectorExceptionTestCase.java
@@ -0,0 +1,217 @@
+/*
+ * [[
+ * Copyright (C) 2001 - 2006 The Software Conservancy as Trustee. All rights
+ * reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Nothing in this notice shall be deemed to grant any rights to
+ * trademarks, copyrights, patents, trade secrets or any other intellectual
+ * property of the licensor or any contributor except as expressly stated
+ * herein. No patent license is granted separate from the Software, for
+ * code that you delete from the Software, or for combinations of the
+ * Software with other software or hardware.
+ * ]]
+ */
+
+package org.openadaptor.core.adaptor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openadaptor.core.adaptor.Adaptor;
+import org.openadaptor.core.connector.TestReadConnector;
+import org.openadaptor.core.connector.TestWriteConnector;
+import org.openadaptor.core.processor.TestProcessor;
+import org.openadaptor.core.router.Router;
+import org.openadaptor.core.router.RoutingMap;
+
+import junit.framework.TestCase;
+
+public class ConnectorExceptionTestCase extends TestCase {
+
+	/**
+	 * test that write connector exceptions are trapped and routed correctly
+	 *
+	 */
+	public void testWriteNodeException() {
+
+		TestReadConnector readNode = new TestReadConnector("i");
+		readNode.setDataString("x");
+		readNode.setMaxSend(5);
+
+		TestProcessor processor = new TestProcessor("p");
+
+		TestWriteConnector writeNode = new TestWriteConnector("o");
+		writeNode.setExpectedOutput(AdaptorTestCase.createStringList("p(x)", 3));
+		writeNode.setExceptionFrequency(2);
+
+		TestWriteConnector errorWriteNode = new TestWriteConnector("e");
+		errorWriteNode.setExpectedOutput(AdaptorTestCase.createStringList("java.lang.RuntimeException:test:p(x)", 2));
+
+		// create router
+		RoutingMap routingMap = new RoutingMap();
+		
+		Map processMap = new HashMap();
+		processMap.put(readNode, processor);
+		processMap.put(processor, writeNode);
+		routingMap.setProcessMap(processMap);
+		
+		Map exceptionMap = new HashMap();
+		exceptionMap.put("java.lang.Exception", errorWriteNode);
+		routingMap.setExceptionMap(exceptionMap);
+    Router router = new Router(routingMap);
+    
+    // create adaptor
+    Adaptor adaptor = new Adaptor();
+    adaptor.setMessageProcessor(router);
+		adaptor.setRunInCallingThread(true);
+
+		// run adaptor
+		adaptor.run();
+		
+		assertTrue(adaptor.getExitCode() == 0);
+
+	}
+	
+	/**
+	 * test that write connector exception causes adaptor to fail if there
+	 * is no exception mapping
+	 *
+	 */
+	public void testWriteNodeException2() {
+
+		TestReadConnector readNode = new TestReadConnector("i");
+		readNode.setDataString("x");
+		readNode.setMaxSend(5);
+
+		TestProcessor processor = new TestProcessor("p");
+
+		TestWriteConnector writeNode = new TestWriteConnector("o");
+		writeNode.setExpectedOutput(AdaptorTestCase.createStringList("p(x)", 1));
+		writeNode.setExceptionFrequency(2);
+
+		// create router
+		RoutingMap routingMap = new RoutingMap();
+		
+		Map processMap = new HashMap();
+		processMap.put(readNode, processor);
+		processMap.put(processor, writeNode);
+		routingMap.setProcessMap(processMap);
+		
+		Map exceptionMap = new HashMap();
+		routingMap.setExceptionMap(exceptionMap);
+    Router router = new Router(routingMap);
+    
+    // create adaptor
+    Adaptor adaptor = new Adaptor();
+    adaptor.setMessageProcessor(router);
+		adaptor.setRunInCallingThread(true);
+
+		// run adaptor
+		adaptor.run();
+		
+		assertTrue(adaptor.getExitCode() != 0);
+	}
+	
+	/**
+	 * test that write connector exception causes adaptor to fail if batch size
+	 * is > 1.
+	 *
+	 */
+	public void testWriteNodeException3() {
+
+		TestReadConnector readNode = new TestReadConnector("i");
+		readNode.setDataString("x");
+		readNode.setMaxSend(5);
+		readNode.setBatchSize(2);
+
+		TestProcessor processor = new TestProcessor("p");
+
+		TestWriteConnector writeNode = new TestWriteConnector("o");
+		writeNode.setExpectedOutput(AdaptorTestCase.createStringList("p(x)", 1));
+		writeNode.setExceptionFrequency(2);
+
+		TestWriteConnector errorWriteNode = new TestWriteConnector("e");
+		errorWriteNode.setExpectedOutput(AdaptorTestCase.createStringList("java.lang.RuntimeException:test:p(x)", 0));
+
+		// create router
+		RoutingMap routingMap = new RoutingMap();
+		
+		Map processMap = new HashMap();
+		processMap.put(readNode, processor);
+		processMap.put(processor, writeNode);
+		routingMap.setProcessMap(processMap);
+		
+		Map exceptionMap = new HashMap();
+		exceptionMap.put("java.lang.Exception", errorWriteNode);
+		routingMap.setExceptionMap(exceptionMap);
+    Router router = new Router(routingMap);
+    
+    // create adaptor
+    Adaptor adaptor = new Adaptor();
+    adaptor.setMessageProcessor(router);
+		adaptor.setRunInCallingThread(true);
+
+		// run adaptor
+		adaptor.run();
+		
+		assertTrue(adaptor.getExitCode() != 0);
+	}
+	
+	/**
+	 * test that read connector exceptions cause the adaptor to fail
+	 *
+	 */
+	public void testReadNodeException() {
+
+		TestReadConnector readNode = new TestReadConnector("i");
+		readNode.setDataString("x");
+		readNode.setMaxSend(5);
+		readNode.setExceptionFrequency(5);
+
+		TestProcessor processor = new TestProcessor("p");
+
+		TestWriteConnector writeNode = new TestWriteConnector("o");
+		writeNode.setExpectedOutput(AdaptorTestCase.createStringList("p(x)", 4));
+
+		// create router
+		RoutingMap routingMap = new RoutingMap();
+		
+		Map processMap = new HashMap();
+		processMap.put(readNode, processor);
+		processMap.put(processor, writeNode);
+		routingMap.setProcessMap(processMap);
+		
+		Map exceptionMap = new HashMap();
+		routingMap.setExceptionMap(exceptionMap);
+    Router router = new Router(routingMap);
+    
+    // create adaptor
+    Adaptor adaptor = new Adaptor();
+    adaptor.setMessageProcessor(router);
+		adaptor.setRunInCallingThread(true);
+
+		// run adaptor
+		adaptor.run();
+		
+		assertTrue(adaptor.getExitCode() != 0);
+	}
+}
---
+++ test/integration/src/org/openadaptor/core/processor/TestProcessor.java
@@ -0,0 +1,114 @@
+/*
+ * [[
+ * Copyright (C) 2001 - 2006 The Software Conservancy as Trustee. All rights
+ * reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Nothing in this notice shall be deemed to grant any rights to
+ * trademarks, copyrights, patents, trade secrets or any other intellectual
+ * property of the licensor or any contributor except as expressly stated
+ * herein. No patent license is granted separate from the Software, for
+ * code that you delete from the Software, or for combinations of the
+ * Software with other software or hardware.
+ * ]]
+ */
+
+package org.openadaptor.core.processor;
+
+import java.util.List;
+
+import org.openadaptor.core.Component;
+import org.openadaptor.core.IDataProcessor;
+
+public class TestProcessor extends Component implements IDataProcessor {
+
+  private Class exceptionClass = RuntimeException.class;
+
+  private int exceptionFrequency = 0;
+
+  private int discardFrequency = 0;
+
+  private int gobbleFrequency = 0;
+
+  private int count = 0;
+
+  public TestProcessor() {
+  }
+
+  public TestProcessor(String id) {
+    super(id);
+  }
+
+  public void setExceptionClassName(String c) {
+    try {
+      exceptionClass = Class.forName(c);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+  public void setExceptionFrequency(int frequency) {
+    this.exceptionFrequency = frequency;
+  }
+
+  public void setDiscardFrequency(int frequency) {
+    this.discardFrequency = frequency;
+  }
+
+  public void setGobbleFrequency(int frequency) {
+    this.gobbleFrequency = frequency;
+  }
+
+  public Object[] process(Object data) {
+    count++;
+
+    if (exceptionFrequency > 0 && (count % exceptionFrequency == 0)) {
+      RuntimeException ex = null;
+      try {
+        ex = (RuntimeException) exceptionClass.newInstance();
+      } catch (Exception e) {
+        throw new RuntimeException("failed to throw runtimeException " + exceptionClass.getName());
+      }
+      throw ex;
+    }
+
+    if (discardFrequency > 0 && (count % discardFrequency == 0)) {
+      return null;
+    }
+
+    if (gobbleFrequency > 0 && (count % gobbleFrequency == 0)) {
+      return new Object[0];
+    }
+
+    StringBuffer buffer = new StringBuffer();
+    buffer.append(getId() != null ? getId() : TestProcessor.class.getName());
+    buffer.append("(").append(data.toString()).append(")");
+    return new Object[] { buffer.toString() };
+  }
+
+  public void reset(Object context) {
+  }
+
+  public void validate(List exceptions) {
+    count = 0;
+  }
+
+}
