c949ddfa7264cda761900c578cdcf09f8a8adfe3
fred <fred@c0ec5643-5123-0410-b01b-e101a1959115>
2007-01-25 17:31:31 +0000

    corrected copyright notices - added ant tasks to check and update
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@252 c0ec5643-5123-0410-b01b-e101a1959115

--- src/org/openadaptor/util/Application.java
+++ src/org/openadaptor/util/Application.java
@@ -1,35 +1,32 @@
 /*
- #* [[
- #* Copyright (C) 2000-2003 The Software Conservancy as Trustee. All rights
- #* reserved.
- #*
- #* Permission is hereby granted, free of charge, to any person obtaining a
- #* copy of this software and associated documentation files (the
- #* "Software"), to deal in the Software without restriction, including
- #* without limitation the rights to use, copy, modify, merge, publish,
- #* distribute, sublicense, and/or sell copies of the Software, and to
- #* permit persons to whom the Software is furnished to do so, subject to
- #* the following conditions:
- #*
- #* The above copyright notice and this permission notice shall be included
- #* in all copies or substantial portions of the Software.
- #*
- #* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- #* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- #* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- #* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- #* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- #* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- #* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- #*
- #* Nothing in this notice shall be deemed to grant any rights to
- #* trademarks, copyrights, patents, trade secrets or any other intellectual
- #* property of the licensor or any contributor except as expressly stated
- #* herein. No patent license is granted separate from the Software, for
- #* code that you delete from the Software, or for combinations of the
- #* Software with other software or hardware.
- #* ]]
- */
+ Copyright (C) 2001 - 2007 The Software Conservancy as Trustee. All rights reserved. 
+                                                                                     
+ Permission is hereby granted, free of charge, to any person obtaining a             
+ copy of this software and associated documentation files (the                       
+"Software"), to deal in the Software without restriction, including                
+ without limitation the rights to use, copy, modify, merge, publish,                 
+ distribute, sublicense, and/or sell copies of the Software, and to                  
+ permit persons to whom the Software is furnished to do so, subject to               
+ the following conditions:                                                           
+                                                                                     
+ The above copyright notice and this permission notice shall be included             
+ in all copies or substantial portions of the Software.                              
+                                                                                     
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS           
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                          
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                               
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE              
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION              
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION               
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                     
+                                                                                     
+ Nothing in this notice shall be deemed to grant any rights to                       
+ trademarks, copyrights, patents, trade secrets or any other intellectual            
+ property of the licensor or any contributor except as expressly stated              
+ herein. No patent license is granted separate from the Software, for                
+ code that you delete from the Software, or for combinations of the                  
+ Software with other software or hardware.                                           
+*/
 
 package org.openadaptor.util;
 
@@ -74,9 +71,8 @@ public class Application {
 
   private static final String BUILD_PROPERTIES_NAME     = ".openadaptor.properties";
 
-  private static final String LICENCE_TEXT = "\n"
-      + " [[                                                                                  \n"
-      + " Copyright (C) 2001 - 2006 The Software Conservancy as Trustee. All rights reserved. \n"
+  public static final String LICENCE_TEXT = 
+        " Copyright (C) 2001 - 2007 The Software Conservancy as Trustee. All rights reserved. \n"
       + "                                                                                     \n"
       + " Permission is hereby granted, free of charge, to any person obtaining a             \n"
       + " copy of this software and associated documentation files (the                       \n"
@@ -102,12 +98,10 @@ public class Application {
       + " property of the licensor or any contributor except as expressly stated              \n"
       + " herein. No patent license is granted separate from the Software, for                \n"
       + " code that you delete from the Software, or for combinations of the                  \n"
-      + " Software with other software or hardware.                                           \n"
-      + " ]]                                                                                  \n"
-      + "                                                                                     \n";
+      + " Software with other software or hardware.                                           \n";
 
   static {
-    System.err.println(LICENCE_TEXT);
+    System.err.println(LICENCE_TEXT + "\n");
   }
   
   public Application() {
