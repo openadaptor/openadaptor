c93ce9cc64477380f7dddb1f01f613b7e9efe9c6
higginse <higginse@c0ec5643-5123-0410-b01b-e101a1959115>
2007-01-31 19:48:35 +0000

    Copyright tool generating false positives on empty lines? Cut/paste from other ones.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@279 c0ec5643-5123-0410-b01b-e101a1959115

--- src/org/openadaptor/auxil/processor/javascript/ScriptableSimpleRecord.java
+++ src/org/openadaptor/auxil/processor/javascript/ScriptableSimpleRecord.java
@@ -1,41 +1,33 @@
+/*
+ Copyright (C) 2001 - 2007 The Software Conservancy as Trustee. All rights reserved. 
+                                                                                     
+ Permission is hereby granted, free of charge, to any person obtaining a             
+ copy of this software and associated documentation files (the                       
+"Software"), to deal in the Software without restriction, including                
+ without limitation the rights to use, copy, modify, merge, publish,                 
+ distribute, sublicense, and/or sell copies of the Software, and to                  
+ permit persons to whom the Software is furnished to do so, subject to               
+ the following conditions:                                                           
+                                                                                     
+ The above copyright notice and this permission notice shall be included             
+ in all copies or substantial portions of the Software.                              
+                                                                                     
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS           
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                          
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                               
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE              
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION              
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION               
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                     
+                                                                                     
+ Nothing in this notice shall be deemed to grant any rights to                       
+ trademarks, copyrights, patents, trade secrets or any other intellectual            
+ property of the licensor or any contributor except as expressly stated              
+ herein. No patent license is granted separate from the Software, for                
+ code that you delete from the Software, or for combinations of the                  
+ Software with other software or hardware.                                           
+*/
 package org.openadaptor.auxil.processor.javascript;
-/* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Rhino code, released
- * May 6, 1998.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1999
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Norris Boyd
- *
- * Alternatively, the contents of this file may be used under the terms of
- * the GNU General Public License Version 2 or later (the "GPL"), in which
- * case the provisions of the GPL are applicable instead of those above. If
- * you wish to allow use of your version of this file only under the terms of
- * the GPL and not to allow others to use your version of this file under the
- * MPL, indicate your decision by deleting the provisions above and replacing
- * them with the notice and other provisions required by the GPL. If you do
- * not delete the provisions above, a recipient may use your version of this
- * file under either the MPL or the GPL.
- *
- * ***** END LICENSE BLOCK ***** */
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
