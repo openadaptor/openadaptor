7ae6a342e3e2a262840176302f30ffb1c115a4d9
fennelr <fennelr@c0ec5643-5123-0410-b01b-e101a1959115>
2007-02-13 10:29:25 +0000

    Added guards for NullPointerExceptions
    Updated javadocs
    
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@370 c0ec5643-5123-0410-b01b-e101a1959115

--- src/org/openadaptor/auxil/connector/jdbc/writer/JDBCWriteConnector.java
+++ src/org/openadaptor/auxil/connector/jdbc/writer/JDBCWriteConnector.java
@@ -27,32 +27,40 @@
 
 package org.openadaptor.auxil.connector.jdbc.writer;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openadaptor.auxil.connector.jdbc.JDBCConnection;
 import org.openadaptor.core.connector.AbstractWriteConnector;
 import org.openadaptor.core.exception.ComponentException;
+import org.openadaptor.core.exception.ConnectionException;
 import org.openadaptor.core.exception.ValidationException;
 import org.openadaptor.core.transaction.ITransactional;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
- * Processes data by executing PreparedStatements against a database. The conversion of the data to a prepared statement
- * is delegate to an IStatementConverter, the default is a StatementConverter (which calls toString() on data, i.e. it
- * assumes that an upstream component has converted the data to valid SQL.
- * 
+ * Processes data by executing PreparedStatements against a database.
+ * <p/>
+ *
+ * The conversion of the data to a prepared statement is delegate to an IStatementConverter, the
+ * default is a StatementConverter which calls toString() on data (i.e. it assumes that an upstream
+ * component has converted the data to valid SQL).
+ *
+ * @author J Perry, Russ Fennell
+ *
  * @see IStatementConverter
+ * @see JDBCConnection
  */
 public class JDBCWriteConnector extends AbstractWriteConnector implements ITransactional {
 
   private static final Log log = LogFactory.getLog(JDBCWriteConnector.class.getName());
 
-  private IStatementConverter statementConverter = new StatementConverter();
 
+  private IStatementConverter statementConverter = new StatementConverter();
   private JDBCConnection jdbcConnection;
+
 
   public JDBCWriteConnector() {
     super();
@@ -62,14 +70,21 @@ public class JDBCWriteConnector extends AbstractWriteConnector implements ITrans
     super(id);
   }
 
+
+  /**
+   * Mandatory
+   *
+   * @param jdbcConnection the connection details for the database.
+   */
   public void setJdbcConnection(final JDBCConnection jdbcConnection) {
     this.jdbcConnection = jdbcConnection;
   }
 
 
   /**
+   * Optional. Defaults to a StatementConverter
+   *
    * @param statementConverter the class that converts the incoming data into a PreparedStatement.
-   * Defaults to a StatementConverter.
    */
   public void setStatementConverter(final IStatementConverter statementConverter) {
     this.statementConverter = statementConverter;
@@ -77,6 +92,8 @@ public class JDBCWriteConnector extends AbstractWriteConnector implements ITrans
 
 
   /**
+   * Checks that all mandatory properties have been set.
+   *
    * @param exceptions list of exceptions that any validation errors will be appended to
    */
   public void validate(List exceptions) {
@@ -85,7 +102,20 @@ public class JDBCWriteConnector extends AbstractWriteConnector implements ITrans
   }
 
 
+  /**
+   * Executes the prepared statement for each of the records in the array.
+   *
+   * @param data the source of the data for the prepared statement
+   *
+   * @return null
+   *
+   * @throws ComponentException just a wrapper around any SQLExceptions that may be thrown
+   * or if the jdbcConnection details have not been set
+   */
   public Object deliver(Object[] data) throws ComponentException {
+    if ( jdbcConnection == null )
+      throw new ConnectionException("No connection details defined. You must supply values for the [jdbcConnection] property", this);
+
     boolean sucess = false;
     while (!sucess) {
       try {
@@ -99,38 +129,72 @@ public class JDBCWriteConnector extends AbstractWriteConnector implements ITrans
         jdbcConnection.handleException(e, null);
       }
     }
+
     return null;
   }
 
+
+  /**
+   * Creates a connection to the database. If a connection already exists then we use it
+   * rather than creating a new one.
+   *
+   * @throws ComponentException just a wrapper around a SQLException or thrown if the
+   * jdbcConnection property has not been set
+   */
   public void connect() throws ComponentException {
     log.debug("Connector: [" + getId() + "] connecting ....");
+
+    if ( jdbcConnection == null )
+      throw new ConnectionException("No connection details defined. You must supply values for the [jdbcConnection] property", this);
+
+    if ( jdbcConnection.isConnected() ) {
+      log.info("Using existing connection");
+      return;
+    }
+
     try {
       jdbcConnection.connect();
       statementConverter.initialise(jdbcConnection.getConnection());
     } catch (SQLException e) {
       jdbcConnection.handleException(e, "Failed to establish JDBC connection");
     }
+
     connected = true;
     log.info("Connector: [" + getId() + "] successfully connected.");
   }
 
+
+  /**
+   * Closes the connectionh to the dattabase
+   *
+   * @throws ComponentException just a wrapper around any SQLexception that may be thrown
+   */
   public void disconnect() throws ComponentException {
     log.debug("Connector: [" + getId() + "] disconnecting ....");
+
+    if ( jdbcConnection == null ) {
+      log.info("Connection already closed");
+      return;
+    }
 
     try {
       jdbcConnection.disconnect();
     } catch (SQLException e) {
       jdbcConnection.handleException(e, "Failed to disconnect JDBC connection");
     }
+
     connected = false;
     log.info("Connector: [" + getId() + "] disconnected");
   }
 
+
+  /**
+   * @return the transaction resource if the connection is transacted or null otherwise
+   */
   public Object getResource() {
-    if (jdbcConnection.isTransacted()) {
+    if ( jdbcConnection != null && jdbcConnection.isTransacted() )
       return jdbcConnection.getTransactionalResource();
-    } else {
-      return null;
-    }
+
+    return null;
   }
 }
