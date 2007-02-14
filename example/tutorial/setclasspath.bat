c9e5cafc2a8f41a940cd66531c88548208bc2413
fred <fred@c0ec5643-5123-0410-b01b-e101a1959115>
2007-02-14 15:00:47 +0000

    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@390 c0ec5643-5123-0410-b01b-e101a1959115

---
+++ example/tutorial/setclasspath.bat
@@ -0,0 +1,6 @@
+set CLASSPATH=.;
+set CLASPATH=%CLASSPATH%;..\..\lib\openadaptor.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\opendaptor-spring.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\openadaptor-depends.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\3rdparty\hsqldb.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\3rdparty\jboss-client_4.0.5.jar
---
+++ example/tutorial/setclasspath.sh
@@ -0,0 +1,7 @@
+CLASSPATH=.
+CLASPATH=${CLASSPATH}:../../lib/openadaptor.jar
+CLASSPATH=${CLASSPATH}:../../lib/opendaptor-spring.jar
+CLASSPATH=${CLASSPATH}:../../lib/openadaptor-depends.jar
+CLASSPATH=${CLASSPATH}:../../lib/3rdparty/hsqldb.jar
+CLASSPATH=${CLASSPATH}:../../lib/3rdparty/jboss-client_4.0.5.jar
+export CLASSPATH
