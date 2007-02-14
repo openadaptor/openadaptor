26cd4ec90d3dad0697944f8292243516dd530a84
fred <fred@c0ec5643-5123-0410-b01b-e101a1959115>
2007-02-14 16:03:31 +0000

    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@391 c0ec5643-5123-0410-b01b-e101a1959115

--- example/tutorial/setclasspath.bat
+++ example/tutorial/setclasspath.bat
@@ -1,6 +1,8 @@
-set CLASSPATH=.;
-set CLASPATH=%CLASSPATH%;..\..\lib\openadaptor.jar
-set CLASSPATH=%CLASSPATH%;..\..\lib\opendaptor-spring.jar
+@echo off
+set CLASSPATH=.
+set CLASSPATH=%CLASSPATH%;..\..\lib\openadaptor.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\openadaptor-spring.jar
 set CLASSPATH=%CLASSPATH%;..\..\lib\openadaptor-depends.jar
 set CLASSPATH=%CLASSPATH%;..\..\lib\3rdparty\hsqldb.jar
-set CLASSPATH=%CLASSPATH%;..\..\lib\3rdparty\jboss-client_4.0.5.jar
+set CLASSPATH=%CLASSPATH%;..\..\lib\3rdparty\jbossall-client.jar
+@echo on
--- example/tutorial/setclasspath.sh
+++ example/tutorial/setclasspath.sh
@@ -1,7 +1,7 @@
 CLASSPATH=.
-CLASPATH=${CLASSPATH}:../../lib/openadaptor.jar
-CLASSPATH=${CLASSPATH}:../../lib/opendaptor-spring.jar
+CLASSPATH=${CLASSPATH}:../../lib/openadaptor.jar
+CLASSPATH=${CLASSPATH}:../../lib/openadaptor-spring.jar
 CLASSPATH=${CLASSPATH}:../../lib/openadaptor-depends.jar
 CLASSPATH=${CLASSPATH}:../../lib/3rdparty/hsqldb.jar
-CLASSPATH=${CLASSPATH}:../../lib/3rdparty/jboss-client_4.0.5.jar
+CLASSPATH=${CLASSPATH}:../../lib/3rdparty/jbossall-client.jar
 export CLASSPATH
