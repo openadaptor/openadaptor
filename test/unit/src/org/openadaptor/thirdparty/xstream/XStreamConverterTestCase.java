aa771876911ad17b5f86f5c8735b76d0f2719488
fred <fred@c0ec5643-5123-0410-b01b-e101a1959115>
2007-03-08 16:18:38 +0000

    expanded tests
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@514 c0ec5643-5123-0410-b01b-e101a1959115

--- test/unit/src/org/openadaptor/thirdparty/xstream/XStreamConverterTestCase.java
+++ test/unit/src/org/openadaptor/thirdparty/xstream/XStreamConverterTestCase.java
@@ -1,6 +1,8 @@
 package org.openadaptor.thirdparty.xstream;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
@@ -20,6 +22,31 @@ public class XStreamConverterTestCase extends TestCase {
     
     Xml2ObjectConverter proc2 = new Xml2ObjectConverter();
     proc2.addAlias("person", p.getClass());
+    proc2.validate(new ArrayList());
+    Person p2 = (Person) proc2.process(xml)[0];
+    
+    assertTrue(p.equals(p2));
+  }
+  
+  public void testMap() {
+    Person p = new Person();
+    p.forename = "donald";
+    p.surname = "duck";
+    p.age = 55;
+    p.insideLeg = 11;
+    
+    Object2XmlConverter processor = new Object2XmlConverter();
+    Map map = new HashMap();
+    map.put("person", p.getClass());
+    processor.setAliasMap(map);
+    processor.validate(new ArrayList());
+    String xml = (String) processor.process(p)[0];
+    
+    Map map2 = new HashMap();
+    map2.put("person", p.getClass().getName());
+    
+    Xml2ObjectConverter proc2 = new Xml2ObjectConverter();
+    proc2.setAliasMap(map2);
     proc2.validate(new ArrayList());
     Person p2 = (Person) proc2.process(xml)[0];
     
