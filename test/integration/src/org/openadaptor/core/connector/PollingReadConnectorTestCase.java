215de5488e035d79f2d83079e79837411d7320f0
higginse <higginse@c0ec5643-5123-0410-b01b-e101a1959115>
2007-10-18 19:13:30 +0000

    Modified tests on (deprecated) PollingReadConnector to run faster
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@958 c0ec5643-5123-0410-b01b-e101a1959115

--- test/integration/src/org/openadaptor/core/connector/PollingReadConnectorTestCase.java
+++ test/integration/src/org/openadaptor/core/connector/PollingReadConnectorTestCase.java
@@ -5,7 +5,11 @@ import org.openadaptor.core.connector.PollingReadConnector;
 import junit.framework.TestCase;
 
 public class PollingReadConnectorTestCase extends TestCase {
-
+  //Cron expression for every second.
+  private static final String CRON_EXP=
+    "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,"+
+    "30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59"+
+    " * * * * ?";
   public void testDefault() {
     TestReadConnector reader = new TestReadConnector("reader");
     reader.setDataString("foobar");
@@ -38,10 +42,10 @@ public class PollingReadConnectorTestCase extends TestCase {
     reader.setDataString("foobar");
     PollingReadConnector poller = new PollingReadConnector("poller");
     poller.setDelegate(reader);
-    poller.setCronExpression("0,5,10,15,20,25,30,35,40,45,50,55 * * * * ?");
-    poller.setPollLimit(6);
+    poller.setCronExpression(CRON_EXP);
+    poller.setPollLimit(3);
     poller.setForceInitialPoll(true);
-    assertTrue(runPoller(poller, reader.getDataString()) == 6);
+    assertTrue(runPoller(poller, reader.getDataString()) == 3);
   }
 
   public void testCronForceInitialPoll() {
@@ -49,7 +53,7 @@ public class PollingReadConnectorTestCase extends TestCase {
     reader.setDataString("foobar");
     PollingReadConnector poller = new PollingReadConnector("poller");
     poller.setDelegate(reader);
-    poller.setCronExpression("0,5,10,15,20,25,30,35,40,45,50,55 * * * * ?");
+    poller.setCronExpression(CRON_EXP);
     poller.setForceInitialPoll(true);
     poller.setPollLimit(2);
     assertTrue(runPoller(poller, reader.getDataString()) == 2);
