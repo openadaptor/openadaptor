1639088b2cf59941cbc13d59923ebc36ff0c7ba0
lachork <lachork@c0ec5643-5123-0410-b01b-e101a1959115>
2007-11-30 15:11:23 +0000

    File based enrichment processor spring example.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@1084 c0ec5643-5123-0410-b01b-e101a1959115

---
+++ example/spring/processors/enrichment/file/CustomFileReadConnector.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+
+import org.openadaptor.auxil.connector.iostream.reader.FileReadConnector;
+import org.openadaptor.auxil.orderedmap.IOrderedMap;
+import org.openadaptor.auxil.orderedmap.OrderedHashMap;
+import org.openadaptor.core.IReadConnector;
+import org.openadaptor.core.IEnrichmentReadConnector;
+import org.openadaptor.core.exception.ConnectionException;
+
+/**
+ * A custom file reader, based on FileReadConnector.
+ */
+public class CustomFileReadConnector extends FileReadConnector implements
+    IEnrichmentReadConnector {
+
+  private String tradeId;
+  
+  private static String [] FIELD_NAMES = new String[]{"TradeId", "BuyOrSell", "Quantity", "Price"};
+  
+  /**
+   * Finds a row with a trade id equal to <code>tradeId</code>.
+   * If found, splits the data into fields.
+   * 
+   * @see IReadConnector#next(long)
+   */
+  public Object[] next(long timeoutMs) {
+    String [] fields = null;
+    boolean found = false;
+    
+    /* Find the relevant record */
+    try {
+      while(!found){
+        Object data = dataReader.read();
+        if (data != null) {
+          String dataS = (String) data;
+          fields = dataS.split(",");
+          if(fields[0].equals(tradeId)){
+            found = true;
+          }
+        } else {
+          break;
+        }
+      }
+    } catch (IOException e) {
+      throw new ConnectionException("IOException, " + e.getMessage(), e, this);
+    }
+    
+    /* Convert string array to ordred map */
+    IOrderedMap fieldsOM = new OrderedHashMap();
+    for(int i=0; i<fields.length; i++){
+      fieldsOM.put(FIELD_NAMES[i], fields[i]);
+    }
+    return new Object[]{fieldsOM};
+  }
+  
+  /**
+   * Sets the tradeId from input parameters.
+   * 
+   * @see IEnrichmentReadConnector#setQueryParameters(IOrderedMap)
+   */
+  public void setQueryParameters(IOrderedMap inputParameters) {
+     tradeId = (String) inputParameters.get(0);
+  }
+}
