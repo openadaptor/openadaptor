c27b6145b7977cf4dbad6b509087a55e8bc1def0
higginse <higginse@c0ec5643-5123-0410-b01b-e101a1959115>
2008-02-26 12:07:37 +0000

    Tidied up Spring config validation task.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@1268 c0ec5643-5123-0410-b01b-e101a1959115

--- support/src/org/openadaptor/util/ant/SpringConfigValidateTask.java
+++ support/src/org/openadaptor/util/ant/SpringConfigValidateTask.java
@@ -23,10 +23,10 @@
  contributor except as expressly stated herein. No patent license is granted separate
  from the Software, for code that you delete from the Software, or for combinations
  of the Software with other software or hardware.
-*/
+ */
 
 package org.openadaptor.util.ant;
- 
+
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
@@ -42,6 +42,11 @@ import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.Reference;
 
+/**
+ * This validates Spring configurations.
+ * @author higginse
+ *
+ */
 public class SpringConfigValidateTask extends Task {
   //If ignoreStubExceptions is set, then this system property will be set to true.
   public static final String IGNORE_STUB_EXCEPTION_FLAG="openadaptor.exception.stub.ignore";
@@ -51,7 +56,7 @@ public class SpringConfigValidateTask extends Task {
   private static String SPRING_URLRESOURCE_CLASSNAME = "org.springframework.core.io.UrlResource";
   private static final String SPRING_GET_BEAN_METHOD = "getBean";
   private static final String SPRING_GET_BEAN_NAMES_METHOD = "getBeanDefinitionNames";
-  
+
   private List filesets = new ArrayList();
   private Path classpath;
   private boolean ignoreStubExceptions=false;
@@ -64,7 +69,7 @@ public class SpringConfigValidateTask extends Task {
     classpath = new Path(getProject());
     classpath.setRefid(r);
   }
-  
+
   /**
    * flag to indicate whether or not {@link StubException} can be ignored.
    * <br>
@@ -79,12 +84,12 @@ public class SpringConfigValidateTask extends Task {
   }
 
   public void execute() throws BuildException {
-
+    int processedFiles=0;
     //
     // create new class loader and use reflection to get constructors and method
     // for instantiating a spring factory and forcing named beans to be loaded
     //
-    
+
     AntClassLoader loader = getProject().createClassLoader(classpath);
     loader.setParent(getProject().getCoreLoader());
     loader.setParentFirst(false);
@@ -96,26 +101,26 @@ public class SpringConfigValidateTask extends Task {
     Constructor factoryConstructor;
     Method getBeanNamesMethod;
     Method getBeanMethod;
-    
+
     try {
       loader.forceLoadClass(SPRING_FACTORY_CLASSNAME);
       Class factoryClass = Class.forName(SPRING_FACTORY_CLASSNAME, true, loader);
       Class resourceClass = Class.forName(SPRING_RESOURCE_CLASSNAME, true, loader);
       Class urlResourceClass = Class.forName(SPRING_URLRESOURCE_CLASSNAME, true, loader);
-      
+
       resourceConstructor = urlResourceClass.getConstructor(new Class[] {String.class});
       factoryConstructor = factoryClass.getConstructor(new Class[] {resourceClass});
-      
+
       getBeanNamesMethod = factoryClass.getMethod(SPRING_GET_BEAN_NAMES_METHOD, new Class[0]);
       getBeanMethod = factoryClass.getMethod(SPRING_GET_BEAN_METHOD, new Class[] {String.class});
-      
+
     } catch (Throwable t) {
       t.printStackTrace();
       throw new BuildException(t);
     }
 
     //Set flag which indicates if stub exceptions may be ignored.
-    System.setProperty("openadaptor.exception.stub.ignore", Boolean.toString(ignoreStubExceptions));
+    System.setProperty(IGNORE_STUB_EXCEPTION_FLAG, Boolean.toString(ignoreStubExceptions));
 
     //
     // iterate thru spring config files, forcing named beans to be loaded
@@ -130,7 +135,7 @@ public class SpringConfigValidateTask extends Task {
         System.setProperty("openadaptor.registration.url", "");
         File dir = fileSet.getDir(getProject());
         String configUrl = "file:" + dir.getAbsolutePath() + "/" + files[i];
-        
+        processedFiles++;
         try {
           Object resource = resourceConstructor.newInstance(new Object[] {configUrl});
           Object factory = factoryConstructor.newInstance(new Object[] {resource});
@@ -145,22 +150,28 @@ public class SpringConfigValidateTask extends Task {
         }
       }
     }
-    
+
     //
     // if any failed then report this and throw an exception
     //
-    
+
     loader.resetThreadContextLoader();
     loader.cleanup();
-    
+
     if (!failedFiles.isEmpty()) {
       for (Iterator iter = failedFiles.iterator(); iter.hasNext();) {
         String filename = (String) iter.next();
-        System.err.println(filename + "!!!");
+        System.err.println("Invalid Spring file: "+filename);
       }
-      throw new BuildException("one or more spring configs are invalid");
+      String msg="Detected "+ failedFiles.size()+" invalid Spring configurations(s) from a total of "+processedFiles+".";
+      System.err.println(msg);
+      throw new BuildException(msg);
+    }
+    else {
+      System.out.println("Processed "+processedFiles+" Spring configurations");
     }
     
+
   }
 
 
