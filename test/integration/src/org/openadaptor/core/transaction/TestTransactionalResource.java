95cc2beef3353db22d5afd1372545731360e3773
lachork <lachork@c0ec5643-5123-0410-b01b-e101a1959115>
2008-06-11 13:09:29 +0000

    Added an optional 'name' property - usuful for debugging.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@1530 c0ec5643-5123-0410-b01b-e101a1959115

--- test/integration/src/org/openadaptor/core/transaction/TestTransactionalResource.java
+++ test/integration/src/org/openadaptor/core/transaction/TestTransactionalResource.java
@@ -6,7 +6,17 @@ import org.openadaptor.core.transaction.ITransactionalResource;
 public class TestTransactionalResource implements ITransactionalResource {
 
   private int committed = 0;
+  private int rolledBack = 0;
   private int pending = 0;
+  
+  private String name;
+
+  public TestTransactionalResource() {
+  }
+  
+  public TestTransactionalResource(String name) {
+    this.name = name;
+  }
 
   public void begin() {
   }
@@ -17,11 +27,16 @@ public class TestTransactionalResource implements ITransactionalResource {
   }
 
   public void rollback(Throwable t) {
+    rolledBack += pending;
     pending = 0;
   }
   
   public int getCommittedCount() {
     return committed;
+  }
+  
+  public int getRolledBackCount() {
+    return rolledBack;
   }
   
   public void incrementRecordCount() {
