dfbed0083c9f8d994f1487dd1c7dbe9b5d8ac103
lachork <lachork@c0ec5643-5123-0410-b01b-e101a1959115>
2008-06-25 13:06:06 +0000

    Registration table and columns renamed.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@1613 c0ec5643-5123-0410-b01b-e101a1959115

--- sql/register/mysql/schema.sql
+++ sql/register/mysql/schema.sql
@@ -1,21 +1,21 @@
 DROP TABLE OA_Register
 GO
 CREATE TABLE OA_Register(
-    pkId                	bigint(20) AUTO_INCREMENT NOT NULL,
-    fldAdaptorName          varchar(255) NOT NULL,
-    fldAdaptorId            varchar(255) NULL,
-    fldAdaptorVersion       varchar(255) NOT NULL,
-    fldHostname         	varchar(255) NOT NULL,
-    fldIpAddress        	varchar(255) NOT NULL,
-    fldOSName               varchar(255) NOT NULL,
-    fldOSVersion        	varchar(255) NOT NULL,
-    fldUsername         	varchar(255) NOT NULL,
-    fldInstallDir           varchar(255) NOT NULL,
-    fldJavaVersion          varchar(255) NOT NULL,
-    fldJavaClasspath        text NOT NULL,
-    fldPropertiesFile       mediumtext NOT NULL,
-    fldAdditionalDetails	varchar(4192) NULL,
-    fldTimestamp        	timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(pkID)
+    Id                	bigint(20) AUTO_INCREMENT NOT NULL,
+    AdaptorName         varchar(255) NOT NULL,
+    AdaptorId           varchar(255) NULL,
+    AdaptorVersion      varchar(255) NOT NULL,
+    Hostname         	varchar(255) NOT NULL,
+    IpAddress        	varchar(255) NOT NULL,
+    OSName              varchar(255) NOT NULL,
+    OSVersion        	varchar(255) NOT NULL,
+    Username         	varchar(255) NOT NULL,
+    InstallDir          varchar(255) NOT NULL,
+    JavaVersion         varchar(255) NOT NULL,
+    JavaClasspath       text NOT NULL,
+    PropertiesFile      mediumtext NOT NULL,
+    AdditionalDetails	varchar(4192) NULL,
+    Timestamp        	timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(Id)
 )
 GO
