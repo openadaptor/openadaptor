a0c95ebe89abcdba97aadf2adaf26ec29910af63
lachork <lachork@c0ec5643-5123-0410-b01b-e101a1959115>
2009-11-02 12:59:27 +0000

    SC95 Msg history testing.
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@2014 c0ec5643-5123-0410-b01b-e101a1959115

--- test/integration/src/org/openadaptor/core/history/MessageHistoryTestCase.java
+++ test/integration/src/org/openadaptor/core/history/MessageHistoryTestCase.java
@@ -69,17 +69,65 @@ public class MessageHistoryTestCase extends TestCase {
 	Map expectedMetadata = new HashMap();
 	expectedMetadata.put(TestComponent.TEST_METADATA_KEY, TestComponent.TEST_METADATA_VALUE);  
 	
-	List history = new ArrayList();
-	history.add(null);
-	history.add(new TestComponent.DummyDataProcessor().getClass().getName());
-	history.add(new TestComponent.MetadataValidatingWriteConnector(expectedMetadata).getClass().getName());
-	expectedMetadata.put(Message.MESSAGE_HISTORY_KEY, history);  
+	List expectedHistory = new ArrayList();
+	expectedHistory.add("UNNAMED_MESSAGE_PROCESSOR");
+	expectedHistory.add(new TestComponent.DummyDataProcessor().getClass().getName());
+	expectedHistory.add(new TestComponent.MetadataValidatingWriteConnector(expectedMetadata).getClass().getName());
+	expectedMetadata.put(Message.MESSAGE_HISTORY_KEY, expectedHistory);  
 	
 	router.setHistoryEnabled(true);
 	IDataProcessor processor = new TestComponent.DummyDataProcessor();
     processMap.put(new TestComponent.TestReadConnector(), processor);
     IWriteConnector writeConnector = new TestComponent.MetadataValidatingWriteConnector(expectedMetadata);
     processMap.put(processor, writeConnector);
+    
+    assertTrue(router.isHistoryEnabled());
+    
+    router.setProcessMap(processMap);
+    TestComponent.DummyExceptionHandler eHandler = new TestComponent.DummyExceptionHandler();
+    assertTrue(eHandler.counter == 0);
+    router.setExceptionProcessor(eHandler);
+    adaptor.run();
+    assertTrue(eHandler.counter == 0);
+  }  
+  
+  
+  /**
+   * Checks recording of message history in a fan-out scenario.
+   * 
+   * Read connector -> dummy processor 1 -> history validating processor write connector 1.
+   *                -> dummy processor 2 -> history validating processor write connector 2.
+   */
+  public void testHistoryFanout(){
+    Map expectedMetadata1 = new HashMap();
+    expectedMetadata1.put(TestComponent.TEST_METADATA_KEY, TestComponent.TEST_METADATA_VALUE);  
+    
+    List expectedHistory1 = new ArrayList();
+    expectedHistory1.add("UNNAMED_MESSAGE_PROCESSOR");
+    expectedHistory1.add(new TestComponent.DummyDataProcessor().getClass().getName());
+    expectedHistory1.add(new TestComponent.MetadataValidatingWriteConnector(expectedMetadata1).getClass().getName());
+    expectedMetadata1.put(Message.MESSAGE_HISTORY_KEY, expectedHistory1);  
+    
+    Map expectedMetadata2 = new HashMap();
+    expectedMetadata2.put(TestComponent.TEST_METADATA_KEY, TestComponent.TEST_METADATA_VALUE);  
+    
+    List expectedHistory2 = new ArrayList();
+    expectedHistory2.add("UNNAMED_MESSAGE_PROCESSOR");
+    expectedHistory2.add(new TestComponent.DummyDataProcessor().getClass().getName());
+    expectedHistory2.add(new TestComponent.MetadataValidatingWriteConnector(expectedMetadata1).getClass().getName());
+    expectedHistory2.add(new TestComponent.DummyDataProcessor().getClass().getName());
+    expectedHistory2.add(new TestComponent.MetadataValidatingWriteConnector(expectedMetadata1).getClass().getName());
+    expectedMetadata2.put(Message.MESSAGE_HISTORY_KEY, expectedHistory1);  
+    
+    router.setHistoryEnabled(true);
+    IDataProcessor processor1 = new TestComponent.DummyDataProcessor();
+    processMap.put(new TestComponent.TestReadConnector(), processor1);
+    IWriteConnector writeConnector1 = new TestComponent.MetadataValidatingWriteConnector(expectedMetadata1);
+    processMap.put(processor1, writeConnector1);
+    IDataProcessor processor2 = new TestComponent.DummyDataProcessor();
+    processMap.put(new TestComponent.TestReadConnector(), processor2);
+    IWriteConnector writeConnector2 = new TestComponent.MetadataValidatingWriteConnector(expectedMetadata2);
+    processMap.put(processor2, writeConnector2);
     
     assertTrue(router.isHistoryEnabled());
     
