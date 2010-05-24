787976ac039a65f81ab461fd5b683815cc71c4da
lachork <lachork@c0ec5643-5123-0410-b01b-e101a1959115>
2010-05-24 20:15:15 +0000

    Updated licence header in .java files (2009 to 2010).
    git-svn-id: https://openadaptor3.openadaptor.org/svn/openadaptor3/trunk@2071 c0ec5643-5123-0410-b01b-e101a1959115

--- src/org/openadaptor/auxil/processor/simplerecord/ConditionProcessor.java
+++ src/org/openadaptor/auxil/processor/simplerecord/ConditionProcessor.java
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2001 - 2009 The Software Conservancy as Trustee. All rights reserved.
+ Copyright (C) 2001 - 2010 The Software Conservancy as Trustee. All rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in the
